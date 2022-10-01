f4deffe91a64a743fa186507656016c46a865e4d
C792 <ians10845@gmail.com>
2022-10-01 20:46:38 +0900

    Base Vote System

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import { useForm } from "react-hook-form";
+import { useState } from 'react'
+
 function Vote1() {
   console.log('1');
 }
 function Vote2() {
   console.log('2');
 }
-// let jsonTest = {
-//   no : 1,
-//   productName : "컴퓨터",
-//   price : 200000
-// };
-
-// ('private/vote.json').val(JSON.stringify(jsonTest));
-// let data = JSON.parse(('private/vote.json').val()); 
 
 export default function Home() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
   return (
     <div>
-    <form>
+    <form onSubmit={handleSubmit((data)=>console.log(JSON.stringify(data)))}>
       <h2>개인 번호와 사유를 쓰세요</h2>
-      {/* <input {...register("id")} type="text" placeholder="ID" />
-      <input {...register("pw")} type="password" placeholder="PW" />
-      <input {...register("age")} type="number" placeholder="AGE" />
-      <input type="submit" /> */}
-      <input {...register("id")} type="text" placeholder='ID' />
+      <input id="ID" type="number" placeholder='ID' {...register("ID")} />
       <hr/>
-      <input {...register("reason")} type="text" placeholder='사유' />   
+      <input id="reason" type="text" placeholder='사유' {...register("reason")} />   
       <hr/>
+      <yay>
+        <button type="submit" onClick={Vote1}>Opt1</button>
+        <button type="submit" onClick={Vote2}>Opt2</button>
+      </yay>
     </form>
-    <section>
-      <button onClick={Vote1}>임준규</button>
-      <button onClick={Vote2}>정준호</button>
-      
-    </section>
     </div>
   )
 }
